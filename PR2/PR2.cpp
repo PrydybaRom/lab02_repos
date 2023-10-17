5b14bd414257c050a2671c3ec6232ec05d62f221
unknown <romanpridiba@gmail.com>
2023-10-17 19:08:22 +0300

    Two branch_02 commit

--- PR2/PR2.cpp
+++ PR2/PR2.cpp
@@ -1,17 +1,28 @@
 
+// Lab_02.cpp
+// < Придиба Роман Орестович >
+// Лабораторна робота № 2.1.
+// Лінійні програми.
+// Варіант 0.3
 #include <iostream>
+#include <cmath>
+
 using namespace std;
 int main()
 {
-	double x; // вхідний параметр
-	//double z1; // результат обчислення 1-го виразу
-	 double z2; // результат обчислення 2-го виразу
-	cout << "x = "; cin >> x;
-	//z1 = 2 * x;
-	 z2 = x + x;
+	double Pi = 4 * atan(1.); // число пі
+	double a; // вхідний параметр
+	double z1; // результат обчислення 1-го виразу
+	double z2; // результат обчислення 2-го виразу
+	 
+	cout << "a = "; cin >> a;
+		
+	z1 = (sin(((Pi / 2) + 3 * a))) / (1 - sin((3 * a) - Pi));
+	z2 = 1.0 / tan((5.0 / 4.0) * Pi + (3.0 / 2.0) * a);
+	
 	cout << endl;
-	//cout << "z1 = " << z1 << endl;
-	 cout << "z2 = " << z2 << endl;
+	cout << "z1 = " << z1 << endl;
+	cout << "z2 = " << z2 << endl;
 	cin.get();
 	return 0;
 }
